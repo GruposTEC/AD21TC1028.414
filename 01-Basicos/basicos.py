5a12ee4df2086adb6fa1b4e1fdfb7364b9a62c51
Antonio Mejia <avmejia@tec.mx>
2021-08-11 14:05:44 -0500

    11 agosto 21

---
+++ 01-Basicos/basicos.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 11 13:26:33 2021
+
+@author: avmejia
+"""
+import math
+
+numero = float(input("Dame un numero : "))
+print(numero)
+print(f'El numero que ingresaste es : {numero:.4f}')
+
+print(f'El numero que ingresaste es : {abs(numero)}')
+
+print(f'El numero que ingresaste es : {math.sqrt(numero)}')
+
+resultado = math.sqrt(numero)
+
+print(resultado)
